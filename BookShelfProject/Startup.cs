7f65a81f7664f36e1fd1b3bd5d55c0caf554eff3
MikolHutnik <mikolhutnik@gmail.com>
2018-07-11 10:54:49 -0400

    first commit

---
+++ BookShelfProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BookShelfProject.Startup))]
+namespace BookShelfProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
